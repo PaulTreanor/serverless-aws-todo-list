acc5ffe11d4c4f558679c50eb3b2d162e6ada39a
Paul Treanor <treanorpaul9@gmail.com>
2021-07-05 09:23:54 +0100

    Removed body-parser dependency

--- serverless-api/handler.js
+++ serverless-api/handler.js
@@ -1,13 +1,12 @@
 const serverless = require('serverless-http');
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const AWS = require("aws-sdk");
 const db = new AWS.DynamoDB.DocumentClient();
 const { v4: uuidv4 } = require('uuid');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.post("/cars", async (req, res) => {
   const data = req.body;
